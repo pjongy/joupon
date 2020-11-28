4494ec70fa6d489e7957bec6bc8ee116cbdc7c14
pjongy <hi.pjongy@gmail.com>
2020-11-28 14:50:28 +0900

    Init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+  kotlin("jvm") version "1.4.10"
+}
+
+group = "com.github.pjongy"
+version = "0.0.1"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+}
