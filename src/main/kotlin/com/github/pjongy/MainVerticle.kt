d2dbb6c1f0a95e38a15e3b1f0490061673792aba
pjongy <hi.pjongy@gmail.com>
2020-12-02 23:50:05 +0900

    Apply exposed table generation

--- src/main/kotlin/com/github/pjongy/MainVerticle.kt
+++ src/main/kotlin/com/github/pjongy/MainVerticle.kt
@@ -3,10 +3,15 @@ package com.github.pjongy
 import com.github.pjongy.di.ClockModule
 import com.github.pjongy.di.DaggerComponent
 import com.github.pjongy.di.VertxModule
+import com.github.pjongy.model.Coupon
+import com.github.pjongy.model.CouponWallet
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.core.http.HttpServer
 import io.vertx.ext.web.Router
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.suspendedTransactionAsync
 import org.slf4j.LoggerFactory
 
 class MainVerticle : AbstractVerticle() {
@@ -18,6 +23,11 @@ class MainVerticle : AbstractVerticle() {
     val joupon = DaggerComponent.builder()
       .clockModule(ClockModule())
       .vertxModule(VertxModule(vertx = vertx)).build()
+    runBlocking {
+      suspendedTransactionAsync(db = joupon.database()) {
+        SchemaUtils.create(Coupon, CouponWallet)
+      }.await()
+    }
     logger.info("joupon service started $host:$port")
     val vertx = joupon.vertx()
     val couponService = joupon.couponService()
