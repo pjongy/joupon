72e435367823f91858c963e3eb7660f310a62638
pjongy <hi.pjongy@gmail.com>
2020-12-06 16:40:52 +0900

    Apply routing handler logger

--- src/main/kotlin/com/github/pjongy/extension/Route.kt
+++ src/main/kotlin/com/github/pjongy/extension/Route.kt
@@ -4,11 +4,15 @@ import com.github.pjongy.exception.AuthorizationRequired
 import com.github.pjongy.exception.Duplicated
 import com.github.pjongy.exception.InvalidParameter
 import com.github.pjongy.exception.PermissionRequired
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Route
 import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+
+val logger: Logger = LoggerFactory.getLogger("router")
 
 suspend fun responseHandler(routingContext: RoutingContext, fn: suspend () -> String) {
   val response = routingContext.response()
@@ -41,6 +45,7 @@ fun Route.coroutineHandler(fn: suspend (RoutingContext) -> String): Route {
         try {
           responseHandler(routingContext = ctx) { fn(ctx) }
         } catch (e: Exception) {
+          logger.error(e)
           ctx.fail(e)
         }
       }
