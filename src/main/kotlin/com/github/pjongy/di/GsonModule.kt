9700e6d6f7acf3de0bff4d14cd000e372a6add9d
pjongy <hi.pjongy@gmail.com>
2020-12-07 22:47:34 +0900

    Make gson to use snakecase_underscore

--- src/main/kotlin/com/github/pjongy/di/GsonModule.kt
+++ src/main/kotlin/com/github/pjongy/di/GsonModule.kt
@@ -1,11 +1,15 @@
 package com.github.pjongy.di
 
+import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 
 @Module
 open class GsonModule {
   @Provides
-  open fun provideGson(): Gson = Gson()
+  open fun provideGson(): Gson = GsonBuilder()
+    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+    .create()
 }
